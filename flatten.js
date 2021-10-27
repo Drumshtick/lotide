490d50631b87ec06a840d9c3e5fa53b93c6b1c5d
Drumshtick <ekodnar@outlook.com>
2021-10-27 00:35:28 +0000

    Added flatten to lotide, which converts a 2D array into a 1D array

---
+++ flatten.js
@@ -0,0 +1,51 @@
+const eqArrays = function(ar1, ar2) {
+  if (!arrayLengthMatch(ar1, ar2)) {
+    return false;
+  } else if (!matchEachArrayElement(ar1, ar2)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const arrayLengthMatch = function(ar1, ar2) {
+  if (ar1.length !== ar2.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const matchEachArrayElement = function(ar1, ar2) {
+  for (let i = 0; i < ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(ar1, ar2) {
+  if (eqArrays(ar1, ar2)) {
+    console.log(`✅✅✅ Assertion Passed: Array 1 [${ar1}] === Array 2 [${ar2}]`);
+  } else {
+    console.log(`🚩🚩🚩 Assertion Failed: Array 1 [${ar1}] !== Array 2 [${ar2}]`);
+  }
+};
+
+const flatten = function(arr) {
+  const flat = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!Array.isArray(arr[i])) {
+      flat.push(arr[i]);
+    } else {
+      for (let j = 0; j < arr[i].length; j++) {
+        flat.push(arr[i][j]);
+      }
+    }
+  }
+  return flat;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, [2, 8], 2, [5, 3, 4], 5, [6]]), [1, 2, 8, 2, 5, 3, 4, 5, 6]);
