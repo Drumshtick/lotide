39fb3b3bc62be86bf7502ff9ef10a319c56b0beb
Drumshtick <ekodnar@outlook.com>
2021-11-01 17:53:04 +0000

    Created modules for asserEqual and head, moved test code from files and placed them in test files in a new test directory

--- assertEqual.js
+++ assertEqual.js
@@ -6,13 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-assertEqual("cookie", "cookie");
-
-assertEqual("cookie", "COOKIES");
-
-assertEqual(12, 12);
-
-assertEqual(12, 12.00001);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual("cookie", "cookie");
+
+assertEqual("cookie", "COOKIES");
+
+assertEqual(12, 12);
+
+assertEqual(12, 12.00001);
