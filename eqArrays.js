ca963ca6f67c4a4bade6f5e28954c77f7075d9e3
Drumshtick <ekodnar@outlook.com>
2021-11-01 18:02:58 +0000

    Refactored eqArrays to work as a module. Created a test file for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚩🚩🚩 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(ar1, ar2) {
   if (!arrayLengthMatch(ar1, ar2)) {
     return false;
@@ -33,8 +25,4 @@ const matchEachArrayElement = function(ar1, ar2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+module.exports = eqArrays;
